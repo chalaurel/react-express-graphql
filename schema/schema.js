29cfadecb57aad2f14422b8f0e69ccbd5bd40796
chalaurel <cm_laurel@yahoo.com>
2019-02-08 12:22:53 +0800

    create schema

--- schema/schema.js
+++ schema/schema.js
@@ -1,16 +1,57 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 const {
     GraphQLObjectType,
     GraphQLString,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLSchema
 } = graphql;
+
+const users = [{
+    id: '23',
+    firstName: 'Billy',
+    age: 23
+}, {
+    id: '24',
+    firstName: 'Sally',
+    age: 23
+}];
 
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
-        id: GraphQLString,
-        name: GraphQLString,
-        age: GraphQLInt
+        id: {
+            type: GraphQLString
+        },
+        firstName: {
+            type: GraphQLString
+        },
+        age: {
+            type: GraphQLInt
+        }
     },
-})
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: {
+                id: {
+                    type: GraphQLString
+                }
+            },
+            resolve(parentValue, args) {
+                return _.find(users, {
+                    id: args.id
+                })
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
